7a6a25235bc33e2243618aa6ac201507387e392c
Atai Borubaev <tenguosu@gmail.com>
2026-05-25 00:58:49 +0600

    Модель Task

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('new', 'Новая'),
+    ('in_progress', 'В процессе'),
+    ('done', 'Сделано')
+]
+
+
+class Task(models.Model):
+    description = models.TextField(
+        null=False,
+        blank=False,
+        verbose_name='Описание'
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='new',
+        verbose_name='Статус'
+    )
+
+    due_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата выполнения'
+    )
+
+    def __str__(self):
+        return f'{self.pk}. {self.description}'
