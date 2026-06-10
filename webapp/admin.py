08e3274cde7176432b9bf70ef2308a0656e112c3
Atai Borubaev <tenguosu@gmail.com>
2026-06-10 22:41:29 +0600

    Статусы и типы задач

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from webapp.models import Task
+from webapp.models import (
+    Task,
+    TaskStatus,
+    TaskType
+)
 
 admin.site.register(Task)
+admin.site.register(TaskStatus)
+admin.site.register(TaskType)
