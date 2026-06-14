e62f44a809535b9521d76cd58c291af86afc38db
Atai Borubaev <tenguosu@gmail.com>
2026-06-14 13:31:12 +0600

    Форма валидации задач

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 from webapp.models import (
     Task,
     TaskStatus,
@@ -28,3 +28,32 @@ class TaskForm(forms.ModelForm):
             'status',
             'task_types'
         )
+
+    def clean_summary(self):
+
+        summary = self.cleaned_data['summary']
+
+        if len(summary) < 5:
+
+            raise ValidationError(
+                'Название задачи должно содержать минимум 5 символов'
+            )
+
+        return summary
+
+    def clean(self):
+
+        cleaned_data = super().clean()
+
+        summary = cleaned_data.get('summary')
+        description = cleaned_data.get('description')
+
+        if summary and description:
+
+            if summary.lower() == description.lower():
+
+                raise ValidationError(
+                    'Описание не должно полностью совпадать с названием'
+                )
+
+        return cleaned_data
