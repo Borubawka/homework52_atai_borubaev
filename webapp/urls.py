9038016d110339d67e24da54c5fa9c5c4df242fe
Atai Borubaev <tenguosu@gmail.com>
2026-06-21 23:25:45 +0600

    Создание задач с привязкой к проекту

--- webapp/urls.py
+++ webapp/urls.py
@@ -13,13 +13,12 @@ from webapp.views import (
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-
-    path( 'project/add/', ProjectCreateView.as_view(), name='project_add'),
-    path( 'project/<int:project_id>/', ProjectDetailView.as_view(), name='project_detail'),
-    path( 'project/<int:project_id>/edit/', ProjectUpdateView.as_view(), name='project_edit'),
-    path( 'project/<int:project_id>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
-    path( 'add/', TaskCreateView.as_view(), name='task_add'),
-    path( 'task/<int:task_id>/', TaskDetailView.as_view(), name='task_detail'),
-    path('task/<int:task_id>/edit/',TaskUpdateView.as_view(),name='task_edit'),
-    path( 'task/<int:task_id>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('project/add/', ProjectCreateView.as_view(), name='project_add'),
+    path('project/<int:project_id>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('project/<int:project_id>/edit/', ProjectUpdateView.as_view(), name='project_edit'),
+    path('project/<int:project_id>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('project/<int:project_id>/task/add/', TaskCreateView.as_view(), name='task_add'),
+    path('task/<int:task_id>/', TaskDetailView.as_view(), name='task_detail'),
+    path('task/<int:task_id>/edit/', TaskUpdateView.as_view(), name='task_edit'),
+    path('task/<int:task_id>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import ( render, redirect, get_object_or_404)
+from django.shortcuts import (render, redirect, get_object_or_404)
 from django.urls import reverse
 from django.db.models import Q
 from django.views import View
-from django.views.generic import ( ListView, FormView, CreateView, UpdateView, DeleteView)
-from webapp.models import ( Task, Project)
-from webapp.forms import ( TaskForm, ProjectForm)
+from django.views.generic import (
+    ListView,
+    FormView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from webapp.models import (Task, Project)
+from webapp.forms import (TaskForm, ProjectForm)
 
 class IndexView(ListView):
 
     model = Project
-
     template_name = 'index.html'
-
     context_object_name = 'projects'
-
     paginate_by = 5
 
     def get_queryset(self):
@@ -53,9 +56,7 @@ class ProjectDetailView(View):
 class ProjectCreateView(CreateView):
 
     model = Project
-
     form_class = ProjectForm
-
     template_name = 'project_create.html'
 
     def get_success_url(self):
@@ -70,9 +71,7 @@ class ProjectCreateView(CreateView):
 class ProjectUpdateView(UpdateView):
 
     model = Project
-
     form_class = ProjectForm
-
     template_name = 'project_update.html'
 
     pk_url_kwarg = 'project_id'
@@ -89,7 +88,6 @@ class ProjectUpdateView(UpdateView):
 class ProjectDeleteView(DeleteView):
 
     model = Project
-
     template_name = 'project_delete.html'
 
     pk_url_kwarg = 'project_id'
@@ -120,12 +118,22 @@ class TaskDetailView(View):
 class TaskCreateView(FormView):
 
     template_name = 'create_task.html'
-
     form_class = TaskForm
 
     def form_valid(self, form):
 
-        task = form.save()
+        project = get_object_or_404(
+            Project,
+            id=self.kwargs['project_id']
+        )
+
+        task = form.save(commit=False)
+
+        task.project = project
+
+        task.save()
+
+        form.save_m2m()
 
         return redirect(
             'task_detail',
@@ -135,7 +143,6 @@ class TaskCreateView(FormView):
 class TaskUpdateView(FormView):
 
     template_name = 'edit_task.html'
-
     form_class = TaskForm
 
     def get_form_kwargs(self):
