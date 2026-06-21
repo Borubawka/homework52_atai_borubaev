3d2d3400b05eaf5ace3730081fde2df34ad9f9a1
Atai Borubaev <tenguosu@gmail.com>
2026-06-22 01:10:22 +0600

    Обновил визуал проекта

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import (render, redirect, get_object_or_404)
 from django.urls import reverse
 from django.db.models import Q
-from django.http import Http404
 from django.views import View
 from django.views.generic import (
     ListView,
